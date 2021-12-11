842b8348345815c0638d81588cf15df68bfd860d
Garrett Hansen <garretthansen2000@gmail.com>
2021-12-11 12:35:17 -0700

    move to index.php

--- index.php
+++ index.php
@@ -8,11 +8,34 @@
 
 
 <body>  
-  <?php echo "<h1>Hello World! (CSS has changed this text to blue!)</h1>" ?>  
-  <img src="img/thedude.jpg" alt="The dude">
-  <?php include("example.php"); ?>
-  <button type="button" onclick="helloJS()">Click Me!</button>
-  
+<h1 class="blog-title">Garrett's Blog Site</h1>
+    <div class="blog-entries-actions">
+        <button class="blog-entry-action-btn add-btn">Add</button>
+    </div>
+    <div class="blog-entries">
+        <div class="blog-entry">
+            <div class="blog-entry-header">
+                <h1 class="blog-entry-title">Test entry</h1>
+                <span class="blog-entry-actions">
+                    <button class="blog-entry-action-btn edit-btn">Edit</button>
+                    <button class="blog-entry-action-btn delete-btn">Delete</button>
+                </span>
+            </div>
+            <hr>
+            <div class="blog-entry-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+        </div>
+        <div class="blog-entry">
+            <div class="blog-entry-header">
+                <h1 class="blog-entry-title">Test entry</h1>
+                <span class="blog-entry-actions">
+                    <button class="blog-entry-action-btn edit-btn">Edit</button>
+                    <button class="blog-entry-action-btn delete-btn">Delete</button>
+                </span>
+            </div>
+            <hr>
+            <div class="blog-entry-content">Test content</div>
+        </div>
+    </div>
 </body>
   <script src="js/main.js"></script>
 </html>
